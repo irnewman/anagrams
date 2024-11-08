ca8b274e780dbe94dece962c4240df0e78b67490
irnewman <ian.newman@usask.ca>
2024-11-08 08:15:42 -0600

    0.0.0.9001
    many overdue updates

--- R/check_double.R
+++ R/check_double.R
@@ -1,11 +1,12 @@
 
+
 #' Check for three of the same letter within a word (avoids bug with? find).
 #'
 #' @param word A word or anagram in string format.
 #'
-#' @return TRUE if three or more of same letter found.
+#' @return TRUE if two or more of same letter found.
 
-check_triple <- function(word) {
+check_double <- function(word) {
 
   # split word into separate letters
   word_letters <- unlist(strsplit(word, ""))
@@ -16,7 +17,7 @@ check_triple <- function(word) {
     word_duplicates <- c(word_duplicates,
                          lengths(regmatches(word, gregexpr(l, word))))
   }
-  fail <- word_duplicates >= 3
+  fail <- word_duplicates >= 2
 
   return(any(fail))
 }
